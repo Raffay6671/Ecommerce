9a360c2e38f624f5955d97142423e791aaa04421
Raffay6671 <rafay.abbasi6671@gmail.com>
2024-09-03 10:59:53 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import NavBar from "./components/navbar/NavBar.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+
+function App() {
+  return (
+    <>
+      <>
+        <NavBar />
+      </>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "../navbar/navbar-module.css";
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="container">
+        {/* <nav className="navbar navbar-expand-lg navbar-light">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  HOME
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  SHOP
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  PAGES
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  ELEMENTS
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav> */}
+
+        <div className="MiddleName hello-world">
+          hello world
+          {/* <img src="src/assets/images/Depot.png" alt="Depot" /> */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
